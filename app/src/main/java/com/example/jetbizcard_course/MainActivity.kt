70252eee101dc1dda30aa14ddf22ceb3892255d8
dilermando.augusto <Dilermando.Augusto-ext@ab-Inbev.com>
2024-03-04 15:04:38 -0300

    14. Creating a Card and Changing Its Properties with Modifier

--- app/src/main/java/com/example/jetbizcard_course/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard_course/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.jetbizcard_course
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetbizcard_course.ui.theme.JetBizCardCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +29,7 @@ class MainActivity : ComponentActivity() {
             JetBizCardCourseTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    CreateBizCard()
                 }
             }
         }
@@ -27,22 +37,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
 fun CreateBizCard() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White,
+            ),
+            elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+        ) {
 
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetBizCardCourseTheme {
-
+        CreateBizCard()
     }
 }
