94424cf9b777d99e67d5f227acefd9e10abed373
dilermando.augusto <Dilermando.Augusto-ext@ab-Inbev.com>
2024-03-04 16:33:48 -0300

    20. Adding the Portfolio Button

--- app/src/main/java/com/example/jetbizcard_course/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard_course/MainActivity.kt
@@ -1,23 +1,37 @@
 package com.example.jetbizcard_course
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetbizcard_course.ui.theme.JetBizCardCourseTheme
@@ -52,10 +66,66 @@ fun CreateBizCard() {
             colors = CardDefaults.cardColors(
                 containerColor = Color.White,
             ),
-            elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
         ) {
-
+            Column(
+                modifier = Modifier
+                    .height(300.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                Divider()
+                CreateInfo()
+                Button(onClick = {
+                    Log.d("Clicked", "Portfolio clicked")
+                }) {
+                    Text(text = "Portfolio")
+                }
+            }
         }
+    }
+}
+
+@Composable
+private fun CreateInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Dilermando A. Sikora",
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Android Composer Progammer",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.titleMedium
+        )
+        Text(
+            text = "di_ler@hotmail.com",
+            modifier = Modifier.padding(3.dp),
+        )
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(width = 0.5.dp, color = Color.LightGray),
+        shadowElevation = 4.dp,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_icon), contentDescription = "profile image",
+            modifier = modifier
+                .size(135.dp)
+                .alpha(0.5f),
+            contentScale = ContentScale.Crop
+        )
+
     }
 }
 
